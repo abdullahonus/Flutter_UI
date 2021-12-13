2ab58e3602bfaeb0643f33b56b7d1b346754481e
abdullahonus <abdullah.onus0@gmail.com>
2021-12-13 14:05:23 +0300

    Update templement

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:day4_actors_profile/Animations/FadeAnimation.dart';
 import 'package:flutter/material.dart';
+////////////////////////////////////////////
+  ///follor For more ig: @Countrol4offical
+  ///@countrolfour@gmail.com
+////////////////////////////////////////////
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  )
-);
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -16,52 +18,107 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      drawer: ListView(
+        padding: EdgeInsets.fromLTRB(0, 0, 90, 0),
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Text('Other Languages',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                )),
+          ),
+          ListTile(
+            title: const Text('Java ',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    backgroundColor: Colors.teal)),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('c# ',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    backgroundColor: Colors.teal)),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
       body: Stack(
         children: <Widget>[
           CustomScrollView(
             slivers: <Widget>[
               SliverAppBar(
                 expandedHeight: 450,
-                backgroundColor: Colors.black,
+                backgroundColor: Colors.white,
                 flexibleSpace: FlexibleSpaceBar(
                   collapseMode: CollapseMode.pin,
                   background: Container(
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/emma.jpg'),
-                        fit: BoxFit.cover
-                      )
-                    ),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/emma.jpg'),
+                            fit: BoxFit.cover)),
                     child: Container(
                       decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          colors: [
-                            Colors.black,
-                            Colors.black.withOpacity(.3)
-                          ]
-                        )
-                      ),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              colors: [
+                            Colors.black.withOpacity(.8),
+                            Colors.black.withOpacity(.1)
+                          ])),
                       child: Padding(
                         padding: EdgeInsets.all(20),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: <Widget>[
-                            FadeAnimation(1, Text("Emma Watson", style: 
-                              TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40)
-                            ,)),
-                            SizedBox(height: 20,),
+                            FadeAnimation(
+                                1,
+                                Text(
+                                  "Flutter",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40),
+                                )),
+                            SizedBox(
+                              height: 20,
+                            ),
                             Row(
                               children: <Widget>[
-                                FadeAnimation(1.2, 
-                                  Text("60 Videos", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                FadeAnimation(
+                                    1.2,
+                                    Text(
+                                      "2 Million developers",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    )),
+                                SizedBox(
+                                  width: 50,
                                 ),
-                                SizedBox(width: 50,),
-                                FadeAnimation(1.3, Text("240K Subscribers", style: 
-                                  TextStyle(color: Colors.grey, fontSize: 16)
-                                ,))
+                                FadeAnimation(
+                                    1.3,
+                                    Text(
+                                      "240K Subscribers",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    ))
                               ],
                             )
                           ],
@@ -78,41 +135,87 @@ class _HomePageState extends State<HomePage> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        FadeAnimation(1.6, Text("Emma Charlotte Duerre Watson was born in Paris, France, to English parents, Jacqueline Luesby and Chris Watson, both lawyers. She moved to Oxfordshire when she was five, where she attended the Dragon School.", 
-                        style: TextStyle(color: Colors.grey, height: 1.4),)),
-                        SizedBox(height: 40,),
-                        FadeAnimation(1.6, 
-                          Text("Born", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Flutter SDK is Google's UI toolkit for crafting apps for mobile, web and desktop. Flutter code is compiled to native ARM machine code using Dart's native compilers. Web Stable. Mobile Ads SDK. Native Performance. Null Safe Code. Flexible UI. Fast Development.",
+                              style:
+                                  TextStyle(color: Colors.black, height: 1.4),
+                            )),
+                        SizedBox(
+                          height: 40,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("April, 15th 1990, Paris, France", style: TextStyle(color: Colors.grey),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Platform",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("Nationality", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Android, iOS, Google Fuchsia, Web platform, Linux, macOS and Windows",
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("British", style: TextStyle(color: Colors.grey),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Type",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("Videos", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Application framework",
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.8, Container(
-                          height: 200,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: <Widget>[
-                              makeVideo(image: 'assets/images/emma-1.jpg'),
-                              makeVideo(image: 'assets/images/emma-2.jpg'),
-                              makeVideo(image: 'assets/images/emma-3.jpg'),
-                            ],
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Videos",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                          1.8,
+                          Container(
+                            height: 200,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: <Widget>[
+                                makeVideo(image: 'assets/images/emma-1.jpg'),
+                                makeVideo(image: 'assets/images/emma-2.jpg'),
+                                makeVideo(image: 'assets/images/emma-3.jpg'),
+                              ],
+                            ),
                           ),
-                        )),
-                        SizedBox(height: 120,)
+                        ),
+                        SizedBox(
+                          height: 120,
+                        ),
                       ],
                     ),
                   )
@@ -121,19 +224,23 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
           Positioned.fill(
-            bottom: 50,
+            bottom: 20,
             child: Container(
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: FadeAnimation(2,
+                child: FadeAnimation(
+                  2,
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 30),
                     height: 50,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.yellow[700]
-                    ),
-                    child: Align(child: Text("Follow", style: TextStyle(color: Colors.white),)),
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.black),
+                    child: Align(
+                        child: Text(
+                      "Follow",
+                      style: TextStyle(color: Colors.white),
+                    )),
                   ),
                 ),
               ),
@@ -144,30 +251,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget makeVideoLink({Text}) {}
   Widget makeVideo({image}) {
     return AspectRatio(
-      aspectRatio: 1.5/ 1,
+      aspectRatio: 1.5 / 1,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover
-          )
-        ),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
         child: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomRight,
-              colors: [
-                Colors.black.withOpacity(.9),
-                Colors.black.withOpacity(.3)
-              ]
-            )
-          ),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.3)
+          ])),
           child: Align(
-            child: Icon(Icons.play_arrow, color: Colors.white, size: 70,),
+            child: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 70,
+            ),
           ),
         ),
       ),
